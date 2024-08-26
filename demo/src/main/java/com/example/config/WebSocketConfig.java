306782508e84f6176d299d9e462a82375e7b05ad
Ashutosh Tiwari <ashutoshtiwari200128@gmail.com>
2024-08-27 00:45:47 +0530

    Commiting

---
+++ demo/src/main/java/com/example/config/WebSocketConfig.java
@@ -0,0 +1,11 @@
+package com.example.config;
+
+import org.springframework.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebSocketConfig {
+    
+}
